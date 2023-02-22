dba2259381f00bddf11cc5e6aad01443ede9bf87
lenz1987 <lenzbyahurwa@gmail.com>
2023-02-22 18:36:16 +0300

    Add project files.

---
+++ BLOW FISH ENCRYPTION/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace BLOW_FISH_ENCRYPTION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnencrypt = new System.Windows.Forms.Button();
+            this.rchtxtencrypt = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnencrypt
+            // 
+            this.btnencrypt.Location = new System.Drawing.Point(22, 162);
+            this.btnencrypt.Name = "btnencrypt";
+            this.btnencrypt.Size = new System.Drawing.Size(282, 47);
+            this.btnencrypt.TabIndex = 0;
+            this.btnencrypt.Text = "Encrypt file";
+            this.btnencrypt.UseVisualStyleBackColor = true;
+            this.btnencrypt.Click += new System.EventHandler(this.btnencrypt_Click);
+            // 
+            // rchtxtencrypt
+            // 
+            this.rchtxtencrypt.Location = new System.Drawing.Point(22, 105);
+            this.rchtxtencrypt.Name = "rchtxtencrypt";
+            this.rchtxtencrypt.Size = new System.Drawing.Size(494, 33);
+            this.rchtxtencrypt.TabIndex = 1;
+            this.rchtxtencrypt.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Decrypt file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(522, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "browse file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rchtxtencrypt);
+            this.Controls.Add(this.btnencrypt);
+            this.Name = "Form1";
+            this.Text = "AES encryption demonstration by lenz byahurwa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnencrypt;
+        private System.Windows.Forms.RichTextBox rchtxtencrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ BLOW FISH ENCRYPTION/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+using System.IO;
+
+/*simple AES application to illustrate AES encryprtion
+ * project by Lenz Byahurwa
+ * 
+ */
+
+
+namespace BLOW_FISH_ENCRYPTION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog opn = new OpenFileDialog() { Filter = "All files|*.*" })
+            {
+                if (opn.ShowDialog() == DialogResult.OK)
+                    rchtxtencrypt.Text = opn.FileName;
+            }
+        }
+        [DllImport("KERNEL32.DLL", EntryPoint = "RtlZeroMemory")]
+        public static extern bool ZeroMemory(IntPtr Destination, int Length);
+        public static byte[] GenerateSalt()
+        {
+            byte[] data = new byte[32];
+            using (RNGCryptoServiceProvider rgnCryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                rgnCryptoServiceProvider.GetBytes(data);
+            }
+            return data;
+        }
+        private void FileEncrypt(string inputFile, string password)
+        {
+            byte[] salt = GenerateSalt();
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            RijndaelManaged AES = new RijndaelManaged();
+            AES.KeySize = 256;//aes 256 bit encryption c#
+            AES.BlockSize = 128;//aes 128 bit encryption c#
+            AES.Padding = PaddingMode.PKCS7;
+            var key = new Rfc2898DeriveBytes(passwords, salt, 50000);
+            AES.Key = key.GetBytes(AES.KeySize / 8);
+            AES.IV = key.GetBytes(AES.BlockSize / 8);
+            AES.Mode = CipherMode.CFB;
+            using (FileStream fsCrypt = new FileStream(inputFile + ".aes", FileMode.Create))
+            {
+                fsCrypt.Write(salt, 0, salt.Length);
+                using (CryptoStream cs = new CryptoStream(fsCrypt, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                    {
+                        byte[] buffer = new byte[1048576];
+                        int read;
+                        while ((read = fsIn.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            cs.Write(buffer, 0, read);
+                        }
+                    }
+                }
+            }
+        }
+        private void FileDecrypt(string inputFileName, string outputFileName, string password)
+        {
+            byte[] passwords = Encoding.UTF8.GetBytes(password);
+            byte[] salt = new byte[32];
+            using (FileStream fsCrypt = new FileStream(inputFileName, FileMode.Open))
+            {
+                fsCrypt.Read(salt, 0, salt.Length);
+                RijndaelManaged AES = new RijndaelManaged();
+                AES.KeySize = 256;//aes 256 bit encryption c#
+                AES.BlockSize = 128;//aes 128 bit encryption c#
+                var key = new Rfc2898DeriveBytes(passwords, salt, 50000);
+                AES.Key = key.GetBytes(AES.KeySize / 8);
+                AES.IV = key.GetBytes(AES.BlockSize / 8);
+                AES.Padding = PaddingMode.PKCS7;
+                AES.Mode = CipherMode.CFB;
+                using (CryptoStream cryptoStream = new CryptoStream(fsCrypt, AES.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    using (FileStream fsOut = new FileStream(outputFileName, FileMode.Create))
+                    {
+                        int read;
+                        byte[] buffer = new byte[1048576];
+                        while ((read = cryptoStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fsOut.Write(buffer, 0, read);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnencrypt_Click(object sender, EventArgs e)
+        {
+            string password = "Jesus2006";
+            GCHandle gCHandle = GCHandle.Alloc(password, GCHandleType.Pinned);
+            FileEncrypt(rchtxtencrypt.Text, password);
+            ZeroMemory(gCHandle.AddrOfPinnedObject(), password.Length * 2);
+            gCHandle.Free();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string password = "Jesus2006";
+            GCHandle gch = GCHandle.Alloc(password, GCHandleType.Pinned);
+            FileDecrypt(rchtxtencrypt.Text + ".aes", rchtxtencrypt.Text, password);
+            ZeroMemory(gch.AddrOfPinnedObject(), password.Length * 2);
+            gch.Free();
+        }
+    }
+}
